7b49bf5d25a1addb1aeeb93cdcb21499ecb88eef
Peeyush <69499613+PeeyushSachan@users.noreply.github.com>
2024-11-01 19:10:27 +0530

    basic CrudwithSqflite is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,254 @@
-import 'package:db_practice/counter_cubit.dart';
-import 'package:db_practice/map_cubit.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:path/path.dart';
+import 'package:db_practice/data/local/db_helper.dart';
 
 void main() {
-  runApp(MyMain());
+  runApp(MyApp());
 }
 
-class MyMain extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MultiBlocProvider(
-       providers: [BlocProvider( create: (context) => CounterCubit(),),
-       BlocProvider(create: (context) => MapCubit())
-      ],
-        child: Dashboard(),
-      ),
+      home: MyAppDash(),
     );
   }
 }
 
-class Dashboard extends StatefulWidget {
+class MyAppDash extends StatefulWidget {
   @override
-  _DashboardState createState() => _DashboardState();
+  State<MyAppDash> createState() => _MyAppDashState();
 }
 
-class _DashboardState extends State<Dashboard> {
+class _MyAppDashState extends State<MyAppDash> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  TextEditingController updateTitleController = TextEditingController();
+  TextEditingController updateDescriptionController = TextEditingController();
+  List<Map<String, dynamic>> allNotes = [];
+
+  DBHelper? mainDB;
+
+  @override
+  void initState() {
+    super.initState();
+    mainDB = DBHelper.getInstance;
+    getInitialNotes();
+  }
+
+  getInitialNotes() async {
+    allNotes = await mainDB!.getAllNotes();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("#: ${context.watch<CounterCubit>().state}"),
+        title: Text("App#1"),
       ),
-      body: context.read<MapCubit>().Data.isNotEmpty
+      body: allNotes.isNotEmpty
           ? ListView.builder(
+              itemCount: allNotes.length,
               itemBuilder: (_, index) {
                 return ListTile(
-                  leading: Text(context.read<MapCubit>().Data[index]['data']),
+                  leading: Text.rich(
+                    TextSpan(
+                        text:
+                            "Sn:${allNotes[index][DBHelper.s_no].toString()}\n",
+                        children: [
+                          TextSpan(text: "In:${index.toString()}"),
+                        ]),
+                  ),
+                  trailing: IconButton(
+                      onPressed: () {
+                        mainDB!.deleteNotes(
+                            rowIndex: allNotes[index][DBHelper.s_no]);
+                        getInitialNotes();
+                      },
+                      icon: Icon(Icons.delete)),
+                  title: Text(allNotes[index][DBHelper.Columntitle]),
+                  subtitle: Text(allNotes[index][DBHelper.columndescription]),
+                  onLongPress: () {
+                    updateTitleController.text =
+                        allNotes[index][DBHelper.Columntitle];
+                        updateDescriptionController.text =
+                        allNotes[index][DBHelper.columndescription];
+                    showModalBottomSheet(
+                        context: context,
+                        builder: (_) {
+                          return Container(
+                            width: double.infinity,
+                            padding: EdgeInsets.all(20),
+                            child: Column(children: [
+                              Text(
+                                "Notes data",
+                                style: TextStyle(
+                                  fontSize: 21,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              TextField(
+                                controller: updateTitleController,
+                                decoration: InputDecoration(
+                                    label: Text("Enter your title"),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(21),
+                                    )),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              TextField(
+                                controller: updateDescriptionController,
+                                decoration: InputDecoration(
+                                    label: Text("update your Description"),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(21),
+                                    )),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  OutlinedButton(
+                                      onPressed: () {
+                                        updateNotesInDB(
+                                            updateIndex: allNotes[index]
+                                                [DBHelper.s_no]);
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text("update")),
+                                  OutlinedButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text("cancel")),
+                                ],
+                              )
+                            ]),
+                          );
+                        });
+                  },
                 );
-              },
-              itemCount: context.read<MapCubit>().state.length,
-            )
-          : Text("No table found"),
-      floatingActionButton: FloatingActionButton.large(
+              })
+          : Text("no notes found"),
+      floatingActionButton: FloatingActionButton(
         onPressed: () {
-          context.read<CounterCubit>().incrementCount();
-          context.read<MapCubit>().changeMent();
-          //or
-          // BlocProvider.of<CounterCubit>(context , listen: false).incrementCount();
+          showModalBottomSheet(
+              context: context,
+              builder: (_) {
+                return Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(20),
+                  child: Column(children: [
+                    Text(
+                      "New Notes data",
+                      style: TextStyle(
+                        fontSize: 21,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(
+                          label: Text("Enter your title"),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(21),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: descriptionController,
+                      decoration: InputDecoration(
+                          label: Text("Enter your Description"),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(21),
+                          )),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        OutlinedButton(
+                            onPressed: () {
+                              addNotesInDB();
+                              Navigator.pop(context);
+                            },
+                            child: Text("add")),
+                        OutlinedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text("cancel")),
+                      ],
+                    )
+                  ]),
+                );
+              });
+
+          // mainDB!.addNote(title: "kumar", desc: "baman");
+          // getInitialNotes();
         },
-        child: Text("ok"),
+        child: Icon(Icons.add),
       ),
     );
+  }
+
+  void addNotesInDB() async {
+    var formTitle = titleController.text.toString();
+    var formDescription = descriptionController.text.toString();
+    bool check = await mainDB!.addNote(title: formTitle, desc: formDescription);
+    String msg;
+
+    getInitialNotes();
+    if (!check) {
+      msg = "note addtion is failed";
+    } else {
+      msg = "note added Successfully";
+      getInitialNotes();
+    }
+
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
+  }
+
+  ////updation function
+
+  void updateNotesInDB({required int updateIndex}) async {
+    // Fetch the updated values from the controllers
+    var updateFormTitle = updateTitleController.text.trim();
+    var updateFormDescription = updateDescriptionController.text.trim();
+
+    // Update the note in the database
+    bool check = await mainDB!.updateNotes(
+        rowIndex: updateIndex,
+        rowTitle: updateFormTitle,
+        rowDescription: updateFormDescription);
+
+    // Prepare a message based on the success/failure of the update
+    String msg;
+    if (check) {
+       getInitialNotes();
+      msg = "Note update successfully";
+         
+    } else {
+      msg = "Note updated failed";
+      // Reload the notes after a successful update
+  
+    }
+
+    // Show a snackbar with the appropriate message
+    if (context.mounted) {
+      // Ensure context is still valid
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
+    }
   }
 }
